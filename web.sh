159e7857fedcec75eff6040bbfb2404a10d4ca06
jayeda80 <jayeda80@hotmail.com>
2022-08-22 13:58:32 -0700

    change name for clarification

---
+++ web.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+
+sleep 30
+
+sudo yum update -y
+
+sudo yum install -y gcc-c++ make
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo -E bash -
+sudo yum install -y nodejs
+
+node -e "console.log('Running Node.js ' + process.version)"
+
+sudo yum install unzip -y
+cd ~/ && unzip exchange-22a-ubuntu.zip
+cd ~/exchange-22a-ubuntu 
+cd web && npm install
+
+sleep 30 
+
+cd ../.. && rm -rf exchange-22a-ubuntu.zip
